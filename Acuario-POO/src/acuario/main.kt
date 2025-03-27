22eb000277ec501afdc1fe118078a4b802edab56
Dnilson Achahuanco Huarilloclla <diego.apaza.q@tecsup.edu.pe>
2025-03-27 14:32:29 -0500

    Añadiendo subclases y Herencia

--- Acuario-POO/src/acuario/main.kt
+++ Acuario-POO/src/acuario/main.kt
@@ -1,12 +1,14 @@
 package acuario
 
 import Acuario.Acuario
+import Acuario.TanqueTorre
 
 fun construirAcuario() {
-    val acuario7 = Acuario(numeroDePeces = 29)
-    acuario7.imprimirTamano()
-    acuario7.volumen = 70
-    acuario7.imprimirTamano()
+    val miAcuario = Acuario(ancho = 25, largo = 25, alto = 40)
+    miAcuario.imprimirTamano()
+    val miTorre = TanqueTorre(diametro = 25, alto = 40)
+    miTorre.imprimirTamano()
+
 
 }
 
